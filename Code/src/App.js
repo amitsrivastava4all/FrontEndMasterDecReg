fb5f8161aa25f01210c43bb7a3c4d7d5f80e74b2
amitsrivastava4all <amitsrivastava4all@gmail.com>
2018-12-27 14:09:56 +0530

    Code

---
+++ Code/src/App.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+import React from 'react';
+import WebResume from './container/WebResume';
+export default class App extends Component{
+  render(){
+      return (
+      
+        
+        <WebResume/>
+       
+      )
+  }
+} 
