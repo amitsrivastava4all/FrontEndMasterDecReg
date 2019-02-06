fc8fd82952b77df91bfbb5f1ef1800315d1773b3
amitsrivastava4all <amitsrivastava4all@gmail.com>
2019-02-06 16:14:15 +0530

    Code

---
+++ Code7/Code3/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+import {HttpClientModule} from '@angular/common/http';
+import {Module2Module} from './module2/module2.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Child1Component,
+    Child2Component
+
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,Module2Module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+constructor(){
+  console.log("I am App Module");
+}
+
+}
