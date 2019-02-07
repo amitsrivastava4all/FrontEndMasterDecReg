ba7ac71366babde3703fbc752f151eb0b7135f2f
amitsrivastava4all <amitsrivastava4all@gmail.com>
2019-02-07 22:07:25 +0530

    Code

---
+++ Code7/Code4/src/app/router.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, OnInit } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Route, ActivatedRoute } from "@angular/router";
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import {FormsModule} from '@angular/forms';
+import { Child1Component } from './contact/child1/child1.component';
+import { Child2Component } from './contact/child2/child2.component';
+const route:Route[] = [
+  {path:'',component:HomeComponent},
+  {path:'contact/:name/:city',component:ContactComponent, children:[{path:'office',component:Child1Component},{path:'home',component:Child2Component}]}
+]
+@NgModule({
+  declarations:[ HomeComponent,
+    ContactComponent,
+    Child1Component,
+    Child2Component],
+  imports:[CommonModule, RouterModule.forRoot(route),FormsModule],
+
+
+
+exports:[RouterModule]
+})
+export class MyRouterModule {
+
+}
